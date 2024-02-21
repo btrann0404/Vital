5ab84a6535a022a4d750409278f8fe8d8434f21c
Andrew Ly <lyandrew2004@gmail.com>
2024-02-20 17:05:57 -0800

    andys changes

--- backend/gemini_request.py
+++ backend/gemini_request.py
@@ -1,26 +1,52 @@
-import pathlib
-import textwrap
+
 import google.generativeai as genai
-from IPython.display import display
-from IPython.display import Markdown
 from flask import Flask, jsonify
+import random
 
 GEMINI_API_KEY = 'AIzaSyAjQJtJPCY7jWHoALMYRl_If2T-tWA7iQk'
 
 app = Flask(__name__)
-@app.route('/api/data', methods=['GET'])
 
-def get_data():
-    
+def generate_recommendation():
+    heart_rate = '70'
+    steps = '7560'
+    hours_of_sleep = '9'
+
     genai.configure(api_key=GEMINI_API_KEY)
-    
     model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content("Can you give me a new phrase up to 5 words that is\
-                                      a recommendation of a task for an elderly person?\
-                                      Make it super simple like 'have you drank water today?'\
-                                      Make it health-conscious or mental-health related.")
-    data = {'recommendation': response.text}
+    recommendation = ''
+
+    if int(heart_rate) < 50:
+        recommendation = 'Heart Rate is dangerously low.\nSeek help if necessary.'
+    elif int(heart_rate) > 160:
+        recommendation = 'Heart Rate is dangerously high.\nSeek help if necessary.'
+    else:
+        response = model.generate_content("Can you give me a short phrase in text that is\
+                                        an accessible recommendation of a task for an elderly person?\
+                                        Make it super simple like 'have you drank water today?'\
+                                        Make it health-conscious or mental-health related.\
+                                        Heart rate is " + heart_rate + " steps are " + steps\
+                                        + " and total sleep is " + hours_of_sleep + " hours.\
+                                            You do not need to consider the health data in your response if it seems \
+                                            normal")
+        recommendation = response.text
+        recommendation = recommendation.replace('"', '') # removes quotes 
     
+    return recommendation, heart_rate, steps, hours_of_sleep
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    recommendation, heart_rate, steps, hours_of_sleep = generate_recommendation()    
+    # if len(recommendation) > 36 and recommendation!= 'Heart Rate is dangerously low.\nSeek help if necessary.' and \
+    #     recommendation != 'Heart Rate is dangerously high.\nSeek help if necessary.':
+    #     recommendation = "Have you drank water today?"
+    data = {
+        'recommendation': recommendation,
+        'heart_rate': heart_rate,
+        'steps': steps,
+        'hours_of_sleep': hours_of_sleep
+    }
+
     return jsonify(data)
 
 if __name__ == '__main__':
--- gdsc360/lib/pages/HealthPage.dart
+++ gdsc360/lib/pages/HealthPage.dart
@@ -1,42 +1,140 @@
 import 'package:flutter/material.dart';
+import 'package:gdsc360/components/health_card.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class Healthpage extends StatelessWidget {
-  const Healthpage({super.key});
+class Healthpage extends StatefulWidget {
+  const Healthpage({Key? key}) : super(key: key);
+
+  @override
+  _HealthpageState createState() => _HealthpageState();
+}
+
+class _HealthpageState extends State<Healthpage> {
+  String recommendation = 'Loading suggestion...';
+  String heartRate = 'Fetching Data!';
+  String steps = 'Fetching Data!';
+  String hoursOfSleep = 'Fetching Data!';
+  bool isChartMode = false; // Add a boolean flag for toggling chart mode
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  void fetchData() async {
+    final response = await http.get(Uri.parse('http://127.0.0.1:5000/api/data'));
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = json.decode(response.body);
+      setState(() {
+        recommendation = data['recommendation'];
+        heartRate = data['heart_rate'];
+        steps = data['steps'];
+        hoursOfSleep = data['hours_of_sleep'];
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
+      
+      body: isChartMode ? _buildChartPage() : _buildHealthPage(),
+    );
+  }
+
+  Widget _buildHealthPage() {
+    return ListView(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: HealthCard(
+                icon: Icons.lightbulb_circle,
+                title: "Your Recommendation",
+                subtitle: recommendation,
+                iconColor: Color.fromARGB(255, 112, 108, 76),
+              ),
+            ),
+            const SizedBox(height: 15),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: HealthCard(
+                icon: Icons.favorite,
+                title: "Heart Rate",
+                subtitle: "Current heart rate: $heartRate beats/minute.\nHealthy Range: 60 bpm - 100 bpm",
+                iconColor: Colors.red,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: HealthCard(
+                icon: Icons.timeline,
+                title: "Blood Sugar",
+                subtitle: "Current blood sugar: 90 mg/dL\nHealthy Range: 70 mg/dL - 100 mg/dL",
+                iconColor: Colors.pink,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: HealthCard(
+                icon: Icons.directions_walk,
+                title: "Steps",
+                subtitle: "Current steps taken: $steps\nHealthy Range: 10,000 steps",
+                iconColor: Colors.blue,
+              ),
+            ),
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isChartMode = true;
+                  });
+                },
+                child: Text('View Charts'),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+
+
+Widget _buildChartPage() {
+  return Scaffold(
+    body: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Expanded(
+        child: SingleChildScrollView(
           child: Column(
             children: [
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.directions_walk),
-                  title: Text('Steps'),
-                  subtitle: Text('8500 today'),
-                ),
+              Image.asset('lib/assets/heartrate.png'),
+              Image.asset('lib/assets/bloodsugar.png'),
+              Image.asset('lib/assets/steps_chart.png'),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    isChartMode = false;
+                  });
+                },
+                child: Text('Back To Health'),
+
               ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.favorite),
-                  title: Text('Heart Rate'),
-                  subtitle: Text('75 bpm'),
-                ),
-              ),
-              Card(
-                child: ListTile(
-                  leading: Icon(Icons.hotel),
-                  title: Text('Sleep'),
-                  subtitle: Text('7 hours last night'),
-                ),
-              ),
-              // Add more health cards here
+              const SizedBox(height: 60,)
             ],
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
+}
+
